e162b4df00ba5f1d37f2fb7e11514f6b7105f323
janaestewart <janae.stewart94@gmail.com>
2021-03-04 13:50:18 -0500

    finished API assignment

---
+++ api-template/js/main.js
@@ -0,0 +1,22 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('#poke-holder-img').src = data.sprites.versions['generation-v']['black-white'].front_default
+        document.querySelector('#poke-name').innerText = data.name
+        document.querySelector('#poke-id').innerText = data.id
+        document.querySelector('#poke-height').innerText = data.height
+        document.querySelector('#poke-type').innerText = data.types[0].type.name
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
